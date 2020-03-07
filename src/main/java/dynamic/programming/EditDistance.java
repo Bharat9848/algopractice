84ddf13ddac4991ed0d2d0c9523489d5b7783ec0
pfloyd <bharat9848@gmail.com>
2020-03-07 12:02:06 +0530

    edit distance path tracing wrong

--- src/main/java/dynamic/programming/EditDistance.java
+++ src/main/java/dynamic/programming/EditDistance.java
@@ -41,8 +41,51 @@ class EditDistance {
         int[][] cost = new int[pattern.length()+1][text.length()+1];
         int[][] parent = new int[pattern.length()+1][text.length()+1];
         Arrays.stream(cost).forEach(row -> Arrays.fill(row,-1));
+        cost[0][0] = 0;
         Arrays.stream(parent).forEach(row -> Arrays.fill(row,-1));
-        return editDistanceDPChar(pattern, text, pattern.length(), text.length(), cost, parent);
+        Arrays.stream(parent).forEach(row -> row[0] = 2);
+        Arrays.fill(parent[0],1);
+        parent[0][0] = 0;
+        Integer result = editDistanceDPChar(pattern, text, pattern.length(), text.length(), cost, parent);
+        printAndParse(parent, pattern, text);
+        return result;
+    }
+
+    private void printAndParse(int[][] parent, String pattern, String text) {
+        int row = parent.length-1, col = parent[0].length-1;
+        StringBuilder edit = new StringBuilder("");
+        while(row >=0 && col >= 0){
+            String editType = transalateToEditType(parent[row][col]);
+            edit.insert(0, editType);
+            switch (editType){
+                case "I" : row--;
+                break;
+                case "M":
+                case "S" : row--; col--;
+                break;
+                case "D" : col--;
+            }
+
+        }
+        System.out.println(pattern);
+        System.out.println(edit.toString());
+        System.out.println(text);
+    }
+
+    private String transalateToEditType(int editType) {
+        String result = "";
+        switch (editType){
+            case 1 : result ="I";
+            break;
+            case 0 : result = "M";
+            break;
+            case 2 : result = "D";
+            break;
+            case 3: result = "S";
+            break;
+            default: throw  new RuntimeException("no idea");
+        }
+        return result;
     }
 
     private Integer editDistanceDPChar(String pattern, String text, int p, int t, int[][] cost, int[][] parent) {
@@ -50,14 +93,17 @@ class EditDistance {
         if(p==0){return t;}
         if(t==0){ return p; }
         int matchCost = editDistanceDPChar(pattern, text, p-1,t-1, cost, parent) + matchCost(pattern.charAt(p-1), text.charAt(t-1));
-        int insertCost = editDistanceDPChar(pattern, text, p-1, t, cost, parent) + 1;
-        int deleteCost = editDistanceDPChar(pattern, text, p, t-1, cost, parent) + 1;
+        int insertCost = editDistanceDPChar(pattern, text, p, t-1, cost, parent) + 1;
+        int deleteCost = editDistanceDPChar(pattern, text, p-1, t, cost, parent) + 1;
         int finalCost = matchCost;
+        parent[p][t] = (matchCost(pattern.charAt(p-1), text.charAt(t-1)) != 0? 3 : 0); //represent match
         if(finalCost > insertCost){
             finalCost = insertCost;
+            parent[p][t] = 1; //represent insert
         }
         if(finalCost > deleteCost) {
             finalCost = deleteCost;
+            parent[p][t] = 2; //represent match
         }
         cost[p][t] = finalCost;
         return finalCost;
@@ -65,7 +111,7 @@ class EditDistance {
 
 
     @ParameterizedTest
-    @CsvSource({"abbccddeeffgg,abbccdeeeffff, 3","catpillarrrrlq, patpillaerrols, 4", "hal, hal, 0", "advise, advice, 1", "he, she, 1", "a, b, 1"})
+    @CsvSource({"you,thou, 2","you should not,thou shalt not, 5","abbccddeeffgg,abbccdeeeffff, 3","catpillarrrrlq, patpillaerrols, 4", "hal, hal, 0", "advise, advice, 1", "he, she, 1", "a, b, 1"})
     public void testDP(String pat, String text, String distance) {
         Assert.assertEquals(editDistanceDP(pat, text), Integer.valueOf(distance));
     }
