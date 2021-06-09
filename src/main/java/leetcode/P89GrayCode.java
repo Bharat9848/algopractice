162c993a7e031874724680a25a5c900cdcde4e3c
pfloyd <bharat9848@gmail.com>
2021-06-09 22:11:33 +0530

    P1334 | find the city

--- src/main/java/leetcode/P89GrayCode.java
+++ src/main/java/leetcode/P89GrayCode.java
@@ -1,15 +1,14 @@
-package bits;
+package leetcode;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 /*
 * Gray code is a binary code where each successive value differ in only one bit, as well as when wrapping around. Gray code is common in hardware so that we don't see temporary spurious values during transitions.Given a number of bits n, generate a possible gray code for it. For example, for n = 2, one gray code would be [00, 01, 11, 10].
 * */
-public class GrayCode {
+public class P89GrayCode {
 
     int[] grayBits(int noOfBits){
         int[] lastList = new int[]{0b0, 0b1};
