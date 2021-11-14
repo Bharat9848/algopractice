bb1f9f55102c787bad635f978ae4f9eecdb2b8c5
pfloyd <bharat9848@gmail.com>
2021-11-14 13:24:20 +0530

    P200 | number of islands

--- src/main/java/graph/NoOfIsland.java
+++ src/main/java/graph/NoOfIsland.java
@@ -1,7 +1,7 @@
 package graph;
 
 /**
- * TODO This problem was asked by Amazon.
+ * Done see class P200NumberofIslands
  *
  * Given a matrix of 1s and 0s, return the number of "islands" in the matrix. A 1 represents land and 0 represents water, so an island is a group of 1s that are neighboring whose perimeter is surrounded by water.
  *
