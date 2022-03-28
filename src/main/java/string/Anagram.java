15c30794464f73871a80b98939eb50f292e5cd69
pfloyd <bharat9848@gmail.com>
2022-03-28 16:35:54 +0530

    Anagram recursive

--- src/main/java/string/Anagram.java
+++ src/main/java/string/Anagram.java
@@ -2,23 +2,48 @@ package string;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import util.Pair;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Anagram {
 
-    Set<String> findAnagram(String str) {
+    Set<String> findAnagram1(String str) {
         return IntStream.range(0, str.length())
                 .mapToObj(index -> new Pair<>(str.charAt(index), cutCharAtIndex(str, index)))
                 .map(p -> new Pair<>(p.getFirst(), findAnagramSubString(p.getSec())))
                 .flatMap(p -> p.getSec().stream().map(s -> prependIndex(s, p.getFirst())).collect(Collectors.toSet()).stream())
                 .collect(Collectors.toSet());
+    }
+
+    Set<String> findAnagram(String str) {
+        return calAllAnagramsRec(str, 0);
+    }
+    // abc -> acb cba bca    n!
+    private Set<String> calAllAnagramsRec(String word, int index){
+        if(index == word.length()){
+            return Collections.emptySet();
+
+        }
+        Set<String> subList = calAllAnagramsRec(word, index +1);
+        Set<String> result = new HashSet<>();
+        if(subList.isEmpty()){
+            return new HashSet<>(){{add(word.charAt(index)+"");}};
+        }
+        for(String ana : subList){
+            for(int i=0; i<ana.length(); i++){
+                String before = ana.substring(0, i);
+                String after = ana.substring(i);
+                result.add(before + word.charAt(index) + after);
+            }
+            result.add(ana + word.charAt(index));
+        }
+        return result;
     }
 
     private String cutCharAtIndex(String str, int index) {
@@ -44,22 +69,14 @@ public class Anagram {
         return str.substring(0, srcIndex) + str.charAt(destIndex) + str.substring(srcIndex+1, destIndex) + str.charAt(srcIndex) + str.substring(destIndex+1);
     }
 
-    @Test
-    public void testEdgeCases(){
-        Assert.assertTrue(findAnagram("").isEmpty());
-        Assert.assertTrue(findAnagram("a").equals(new HashSet<>(){{add("a");}}));
-    }
-
-    @Test
-    public void testNormalString(){
-        System.out.println(findAnagram("abcde"));
-        Assert.assertEquals(findAnagram("eat"), new HashSet<>() {{
-            add("eat");
-            add("ate");
-            add("tea");
-            add("aet");
-            add("eta");
-            add("tae");
-        }});
+    @ParameterizedTest
+    @CsvSource(delimiter = '|',value = {
+            "ab|ba,ab",
+            "eat|eat,tae,ate,tea,aet,eta",
+            "a|a"
+    })
+    void testNormalString(String word, String expectedStr){
+//        System.out.println(findAnagram("abcde"));
+        Assert.assertEquals(Arrays.stream(expectedStr.split(",")).collect(Collectors.toSet()),findAnagram(word)) ;
     }
 }
