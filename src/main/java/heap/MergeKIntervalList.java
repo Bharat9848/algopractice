5d22afcdefceca13b0909e52ef448568e29236e5
bharat <bharat9848@gmail.com>
2024-08-09 11:04:01 +0530

    format and others

--- src/main/java/heap/MergeKIntervalList.java
+++ src/main/java/heap/MergeKIntervalList.java
@@ -13,9 +13,9 @@ import java.util.stream.Collectors;
 
 class MergeKIntervalList {
 
-    List<Pair<Integer, Integer>> mergeIntervalKList(List<List<Pair<Integer, Integer>>> kList){
+    List<Pair<Integer, Integer>> mergeIntervalKList(List<List<Pair<Integer, Integer>>> kList) {
 
-        if(kList ==null|| kList.size() <= 1){
+        if (kList == null || kList.size() <= 1) {
             return kList.get(0);
         }
 
@@ -26,20 +26,21 @@ class MergeKIntervalList {
                 return pair1.getSec().getSec() - pair2.getSec().getSec();
             } else {
                 return 1;
-            } });
+            }
+        });
         //init heap
         for (int i = 0; i < kList.size(); i++) {
-            List<Pair<Integer,Integer>> list = kList.get(i);
+            List<Pair<Integer, Integer>> list = kList.get(i);
             Pair<Integer, Integer> ele = list.remove(0);
             heap.offer(new Pair<>(list, ele));
         }
         List<Pair<Integer, Integer>> mergedList = new ArrayList<>();
-        while(!heap.isEmpty()){
-            Pair<List<Pair<Integer, Integer>>,Pair<Integer, Integer>> listToMinEle = heap.remove();
-            Pair<Integer, Integer> minEle =listToMinEle.getSec();
+        while (!heap.isEmpty()) {
+            Pair<List<Pair<Integer, Integer>>, Pair<Integer, Integer>> listToMinEle = heap.remove();
+            Pair<Integer, Integer> minEle = listToMinEle.getSec();
             List<Pair<Integer, Integer>> list = listToMinEle.getFirst();
             mergeIntervals(mergedList, minEle);
-            if(!list.isEmpty()){
+            if (!list.isEmpty()) {
                 Pair<Integer, Integer> nextEle = list.remove(0);
                 heap.offer(new Pair<>(list, nextEle));
             }
@@ -49,20 +50,20 @@ class MergeKIntervalList {
     }
 
     private void mergeIntervals(List<Pair<Integer, Integer>> mergedList, Pair<Integer, Integer> element) {
-        if(mergedList.isEmpty()){
+        if (mergedList.isEmpty()) {
             mergedList.add(element);
-        }else{
+        } else {
             Pair<Integer, Integer> lastElement = mergedList.get(mergedList.size() - 1);
-            if(overlapping(lastElement, element)){
-                mergedList.remove(mergedList.size()-1);
+            if (overlapping(lastElement, element)) {
+                mergedList.remove(mergedList.size() - 1);
                 mergedList.add(new Pair<>(lastElement.getFirst(), element.getSec()));
-            }else {
+            } else {
                 mergedList.add(element);
             }
         }
     }
 
-    private boolean overlapping(Pair<Integer, Integer> first, Pair<Integer, Integer>sec){
+    private boolean overlapping(Pair<Integer, Integer> first, Pair<Integer, Integer> sec) {
         return first.getSec() >= sec.getFirst();
     }
 
@@ -72,22 +73,22 @@ class MergeKIntervalList {
             "[[[1,3],[6,9]]-[[2,5]]-[[9,11]]]|[[1,5],[6,11]]",
             "[[[1,3],[6,9]]-[[2,5]]]|[[1,5],[6,9]]",
     })
-    public void test(String kListOfList, String expected){
+    public void test(String kListOfList, String expected) {
         String[] klists = kListOfList.split("-");
-        List<List<Pair<Integer,Integer>>> arg =
-        Arrays.stream(klists).map(listStr -> Arrays.stream(listStr.split("],\\["))
-                .map(str -> str.replaceAll("\\[", "").replaceAll("]", "").trim())
-                .filter(s -> !s.isEmpty())
-                .map(s -> {
-                    String[] numbers = s.split(",");
-                    return new Pair<>(Integer.parseInt(numbers[0]),Integer.parseInt(numbers[1]));
-                }).collect(Collectors.toList())).collect(Collectors.toList());
+        List<List<Pair<Integer, Integer>>> arg =
+                Arrays.stream(klists).map(listStr -> Arrays.stream(listStr.split("],\\["))
+                        .map(str -> str.replaceAll("\\[", "").replaceAll("]", "").trim())
+                        .filter(s -> !s.isEmpty())
+                        .map(s -> {
+                            String[] numbers = s.split(",");
+                            return new Pair<>(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1]));
+                        }).collect(Collectors.toList())).collect(Collectors.toList());
         List<Pair<Integer, Integer>> expectation = Arrays.stream(expected.split("],\\["))
                 .map(str -> str.replaceAll("\\[", "").replaceAll("]", "").trim())
                 .filter(s -> !s.isEmpty())
                 .map(s -> {
                     String[] numbers = s.split(",");
-                    return new Pair<>(Integer.parseInt(numbers[0]),Integer.parseInt(numbers[1]));
+                    return new Pair<>(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1]));
                 }).collect(Collectors.toList());
         Assertions.assertEquals(expectation, mergeIntervalKList(arg));
     }
--- src/main/resources/DynamicProgramming.md
+++ src/main/resources/DynamicProgramming.md
@@ -1,13 +1,13 @@
 Think of solution in terms of sub solution. 
 
 Following are the ideas where DP are applicable
-1. The problem can be broken down into "overlapping subproblems" - smaller versions of the original problem that are re-used multiple times.
+1. The problem can be broken down into "overlapping sub-problems" - smaller versions of the original problem that are reused multiple times.
 
-2.The problem has an "optimal substructure" - an optimal solution can be formed from optimal solutions to the overlapping subproblems of the original problem.
+2.The problem has an "optimal substructure" - an optimal solution can be formed from optimal solutions to the overlapping sub-problems of the original problem.
 
 It is sometimes difficult to recognize the optimal substructure in the problem statement. Following are some of the characteristic or hint that you have to recognize to classify problem that can be solved by Dynamic programming.
-- problem ask for some optimal solution like whats the maximum or minimum or how many ways etc.
-- Local decision to distinguish between greedy algorithm from DP approach. Greedy algorithm do not need DP approach for the optimal solution like minimum spanning tree etc. On the other hand DP is valid for algorithm where greedy approach will lead to non optimal global solution e.g. knapsack etc.
+- problem ask for some optimal solution like what is the maximum or minimum or how many ways etc.
+- Local decision to distinguish between greedy algorithm from DP approach. Greedy algorithms do not need DP approach for the optimal solution like minimum spanning tree etc. On the other hand DP is valid for algorithm where greedy approach will lead to non-optimal global solution e.g. knapsack etc.
 
 -Space and time complexity is equal to the number of the states and its calculation.
 Note: Check if problems is not straight forward DP check assume it can be solved by greedy approach and then think of any counter example. If counter example is there that means solution need be exhaustive and DP can help in that.
